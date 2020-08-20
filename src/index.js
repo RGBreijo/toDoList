67213d9162396acd5301d03c4f9dc473127d5540
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-08-20 11:18:25 -0400

    tes for card creation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import {toDoListCard} from "./toDoCard.js";
+
+
+let test = new toDoListCard("Jim", "Joke", "immediate", "aug 25"); 
+
+
+// title, textContent, priorityLevel, dueDate
+console.log(test.createToDoListCard("Phsyics", "Complete physics homework", "High", "Aug 25")); 
+
