5787ecc9f5b786de01625280afcff8051105afc7
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-08-20 13:45:16 -0400

    returns input from to do card form

---
+++ src/grabToDoCardInput.js
@@ -0,0 +1,79 @@
+function initializeBtn()
+{
+    const CREATE_BTN = document.querySelector("#createToDoBtn"); 
+    CREATE_BTN.addEventListener("click", getToDoFormInformation);  
+}
+
+
+function getToDoFormInformation()
+{
+    const TITLE  = getTitle(); 
+    const DESCRIPTION = getDiscription(); 
+    const PRIORITY = getPriority(); 
+    const DUE_DATE = getDueDate();  
+
+    return{TITLE, DESCRIPTION, PRIORITY, DUE_DATE};
+}
+
+
+
+/**
+ * Grab the content of the title in the to do input form 
+ */
+function getTitle()
+{
+    const TITLE_ELEMENT = document.querySelector("#title"); 
+    return TITLE_ELEMENT.value;
+}
+
+
+
+/**
+ * Grab the content of the description in the to do input form 
+ */
+function getDiscription()
+{
+    const DESCRIPTION_ELEMENT = document.querySelector("#toDoDescription"); 
+    return DESCRIPTION_ELEMENT.value; 
+}
+
+
+
+/**
+ * Grab the priority in the to do input form 
+ */
+function getPriority()
+{
+    const PRIORITY_ELEMENTS = Array.from(document.querySelectorAll(".priorityLevel")); 
+    let priorityLvlPicked = null;
+
+    for (let prioritylvlElements = 0; prioritylvlElements < PRIORITY_ELEMENTS.length; prioritylvlElements++)
+    {
+        if(PRIORITY_ELEMENTS[prioritylvlElements].checked)
+        {
+            priorityLvlPicked = PRIORITY_ELEMENTS[prioritylvlElements].value; 
+        }
+    }
+
+    if(priorityLvlPicked != null)
+    {
+        return priorityLvlPicked;
+    }else
+    {
+        return;
+    }
+}
+
+
+/**
+ * Get the due date 
+ */
+function getDueDate()
+{
+    const DUE_DATE = document.querySelector("#dueDate"); 
+    return DUE_DATE.value; 
+}
+
+export{initializeBtn};
+
+
