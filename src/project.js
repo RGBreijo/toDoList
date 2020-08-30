4ca00eaa3a3e77c06eb4e50ae6b3961b35e4a659
Raul Garcia Breijo <raulgarcia10000@gmail.com>
2020-08-30 15:36:27 -0400

    skeleton of a new project

---
+++ src/project.js
@@ -0,0 +1,59 @@
+
+
+    function createNewProject(name)
+    {
+        let encapsulateProject = createDiv("sidebarProject");
+        let projectName = createParagraph(name, "selectedProjectTitle"); 
+        encapsulateProject.appendChild(projectName); 
+
+        let encapsulateOptions = createDiv();
+        let edit = createParagraph("Edit"); 
+        let deleteProject = createParagraph("Delete", "deleteProject"); 
+
+        encapsulateOptions.appendChild(edit); 
+        encapsulateOptions.appendChild(deleteProject); 
+
+        encapsulateProject.appendChild(encapsulateOptions);
+
+        return encapsulateProject; 
+    }
+
+
+    
+    /**
+     * Creates a div element 
+     * 
+     * @param {*} divClass Optional class name for the div element 
+     */
+    function createDiv(divClass)
+    {
+        let divElement = document.createElement("div"); 
+
+        if(divClass != undefined)
+        {
+            divElement.setAttribute("class", divClass);
+        }
+        return divElement; 
+    }
+
+
+    /**
+     * Create a paragraph element 
+     * 
+     * @param {c} paragraphContent Text to be put into the paragraph element 
+     * @param {*} paragraphClass Optional class name for the paragraph element 
+     */
+    function createParagraph(paragraphContent, paragraphClass)
+    {
+        let paragraphElement = document.createElement("p");
+        paragraphElement.textContent = paragraphContent;
+
+        if (paragraphClass != undefined)
+        {
+            paragraphElement.setAttribute("class", paragraphClass); 
+        }
+
+        return paragraphElement;
+    }
+
+    export{createNewProject};
